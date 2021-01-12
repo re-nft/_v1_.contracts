25024732d3a724ff78dabfc9b99af3081cb74693
nazariyv <nazariy@inbox.ru>
2021-01-12 11:53:02 +0000

    [*]

--- test/RentNft.test.ts
+++ test/RentNft.test.ts
@@ -6,13 +6,13 @@ import {Resolver as ResolverT} from '../frontend/src/hardhat/typechain/Resolver'
 import {ERC20 as ERC20T} from '../frontend/src/hardhat/typechain/ERC20';
 import {MyERC721 as ERC721T} from '../frontend/src/hardhat/typechain/MyERC721';
 import {Utils as UtilsT} from '../frontend/src/hardhat/typechain/Utils';
+import {SignerWithAddress} from 'hardhat-deploy-ethers/dist/src/signer-with-address';
 
 // default values
 const MAX_RENT_DURATION = 1;
 const DAILY_RENT_PRICE = 2;
 const NFT_PRICE = 3;
 const PAYMENT_TOKEN = 2;
-const GAS_SPONSOR = ethers.constants.AddressZero;
 
 const getEvents = (events: Event[], name: string) => {
   return events.filter((e) => e?.event?.toLowerCase() === name.toLowerCase());
@@ -44,7 +44,7 @@ const setup = deployments.createFixture(async () => {
     ERC20: myERC20,
     ERC721: myERC721,
     Utils: utils,
-    signers: signers.map((acc, ix) => ({[ix]: acc})),
+    signers,
     deployer,
     beneficiary,
   };
@@ -211,11 +211,15 @@ describe('RentNft', function () {
     let RentNft: RentNftT;
     let ERC721: ERC721T;
     let deployer: string;
+    let dude: SignerWithAddress;
+    let lady: SignerWithAddress;
     beforeEach(async () => {
       const setupObj = await setup();
       RentNft = setupObj.RentNft;
       ERC721 = setupObj.ERC721;
       deployer = setupObj.deployer;
+      dude = setupObj.signers[1];
+      lady = setupObj.signers[2];
     });
     // todo: test with various paymentTokens in a single array?
     const lendBatch = async ({
@@ -224,12 +228,10 @@ describe('RentNft', function () {
       maxRentDurations = [],
       dailyRentPrices = [],
       nftPrices = [],
-      expectedLendingIds = [],
     }: lendBatchArgs & {paymentTokens: number[]}) => {
       let _maxRentDurations = maxRentDurations;
       let _dailyRentPrices = dailyRentPrices;
       let _nftPrices = nftPrices;
-      let _expectedLendingIds = expectedLendingIds;
       if (maxRentDurations.length === 0) {
         _maxRentDurations = Array(tokenIds.length).fill(MAX_RENT_DURATION);
       }
@@ -238,9 +240,6 @@ describe('RentNft', function () {
       }
       if (nftPrices.length === 0) {
         _nftPrices = Array(tokenIds.length).fill(NFT_PRICE);
-      }
-      if (expectedLendingIds.length === 0) {
-        _expectedLendingIds = tokenIds.map((v, ix) => ix + 1);
       }
       const txn = await RentNft.lend(
         Array(tokenIds.length).fill(ERC721.address),
@@ -253,35 +252,19 @@ describe('RentNft', function () {
       const receipt = await txn.wait();
       const e = getEvents(receipt.events ?? [], 'Lent');
       expect(e.length).to.eq(tokenIds.length);
-      for (let i = 0; i < tokenIds.length; i++) {
-        const event = e[i].args;
-        if (!event) throw new Error('No args');
-        const {
-          nftAddress,
-          tokenId: _tokenId,
-          lendingId,
-          lenderAddress,
-          maxRentDuration,
-          dailyRentPrice,
-          nftPrice,
-          paymentToken,
-        } = event;
-        expect(nftAddress).to.eq(ERC721.address);
-        expect(_tokenId).to.eq(tokenIds[i]);
-        expect(lendingId).to.eq(_expectedLendingIds[i]);
-        expect(lenderAddress).to.eq(deployer);
-        expect(maxRentDuration).to.eq(MAX_RENT_DURATION);
-        expect(dailyRentPrice).to.eq(DAILY_RENT_PRICE);
-        expect(nftPrice).to.eq(NFT_PRICE);
-        expect(paymentToken).to.eq(paymentTokens[i]);
-        const newNftOwner = await ERC721.ownerOf(tokenIds[i]);
-        expect(newNftOwner).to.eq(RentNft.address);
-      }
     };
     it('rents ok', async () => {
       const tokenIds = [1];
       const eth = 1;
-      await lendBatch({tokenIds, paymentTokens: [eth]});
+      await lendBatch({tokenIds, paymentTokens: [eth], maxRentDurations: [3]});
+      const renftD = (await ethers.getContract('RentNft', dude)) as RentNftT;
+      const _nft = [ERC721.address];
+      const _tokenId = [1];
+      const _id = [1];
+      const _rentDuration = [2];
+      // todo: test for max rent duration exceeded
+      const tx = await renftD.rent(_nft, _tokenId, _id, _rentDuration);
+      const receipt = await tx.wait();
     });
   });
   context('Returning', async function () {});
