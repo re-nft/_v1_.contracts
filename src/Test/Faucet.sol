df8142cd3c1e19be3dddec69d7a8d3f7b2278169
nazariyv <nazariy@inbox.ru>
2021-01-18 21:32:00 +0000

    [*] code checks

--- src/Test/Faucet.sol
+++ src/Test/Faucet.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract Faucet {
     function requestToken(address _token) public {
         IERC20 token = IERC20(_token);
