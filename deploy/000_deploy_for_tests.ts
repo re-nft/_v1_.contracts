15c978b37d58f1ac5a9b8e9f81e0dfec4f5b3bd3
naz <nazariy@inbox.ru>
2021-02-28 14:13:45 +0000

    [+] deploy for dev env changed

--- deploy/000_deploy_for_tests.ts
+++ deploy/000_deploy_for_tests.ts
@@ -1,10 +1,12 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
+import { ERC20 } from '../frontend/src/hardhat/typechain/ERC20';
+import { Resolver } from '../frontend/src/hardhat/typechain/Resolver';
 
 const for_development = async (hre: HardhatRuntimeEnvironment) => {
   // mint all the nfts to lender
   const { getNamedAccounts, ethers } = hre;
-  const { lender } = await getNamedAccounts();
+  const { lender, deployer, beneficiary, renter } = await getNamedAccounts();
 
   const erc721 = await ethers.getContract('MyERC721', lender);
   // const erc1155 = await ethers.getContract('MyERC1155', lender);
@@ -12,6 +14,22 @@ const for_development = async (hre: HardhatRuntimeEnvironment) => {
   for (let i = 0; i < 10; i++) {
     await erc721.award();
   }
+
+  // also send through erc20 balances to everyone
+  const erc20 = ((await ethers.getContract(
+    'MyERC20',
+    deployer
+    /* eslint-disable-next-line */
+  )) as any) as ERC20;
+  const amtToSend = ethers.utils.parseEther('100');
+  await erc20.transfer(lender, amtToSend);
+  await erc20.transfer(beneficiary, amtToSend);
+  await erc20.transfer(renter, amtToSend);
+
+  const resolver = ((await ethers.getContract(
+    'Resolver'
+  )) as unknown) as Resolver;
+  await resolver.setPaymentToken(2, erc20.address);
 };
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
