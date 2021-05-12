46793a12ba06e22b802ccbb6976100d5a30d871c
nazariyv <nazariy@inbox.ru>
2021-05-12 19:34:15 +0100

    changed the test 721 contracts

--- src/Test/E721.sol
+++ src/Test/E721.sol
@@ -1,50 +1,58 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.0;
 
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import '@openzeppelin/contracts/access/Ownable.sol';
 
-contract E721 is IERC721Metadata, ERC721Enumerable {
-    event NewFace(
-        address indexed owner,
-        uint256 indexed tokenId,
-        string tokenURI
-    );
+contract E721 is Ownable, ERC721Enumerable {
+
     uint256 private counter = 0;
-    string[] private tokenUris = [
-        "https://nft.service.cometh.io/3000013",
-        "https://nft.service.cometh.io/3000015",
-        "https://nft.service.cometh.io/6000327",
-        "https://nft.service.cometh.io/6000328",
-        "https://nft.service.cometh.io/6000329",
-        "https://rope.lol/api/RMU/51",
-        "https://rope.lol/api/RMU/52",
-        "https://api.bccg.digital/api/bccg/9",
-        "https://api.bccg.digital/api/bccg/11",
-        "https://api.bccg.digital/api/bccg/1"
-    ];
+    using Strings for uint256;
 
-    constructor() ERC721("E721", "E721") {
-        this;
+    // Optional mapping for token URIs
+    mapping (uint256 => string) private _tokenURIs;
+
+    // Base URI
+    string private _baseURIextended;
+
+    constructor()
+        ERC721("E721", "E721")
+    {
+        _tokenURIs[1] = "https://api.bccg.digital/api/bccg/1";
+        _tokenURIs[2] = "https://api.bccg.digital/api/bccg/2";
+        _tokenURIs[3] = "https://api.bccg.digital/api/bccg/3";
+        _tokenURIs[4] = "https://api.bccg.digital/api/bccg/4";
+        _tokenURIs[5] = "https://api.bccg.digital/api/bccg/5";
+        _tokenURIs[6] = "https://api.bccg.digital/api/bccg/6";
+        _tokenURIs[7] = "https://api.bccg.digital/api/bccg/7";
+        _tokenURIs[8] = "https://api.bccg.digital/api/bccg/8";
+        _tokenURIs[9] = "https://api.bccg.digital/api/bccg/9";
+        _tokenURIs[10] = "https://api.bccg.digital/api/bccg/10";
+    }
+
+    function setBaseURI(string memory baseURI_) external onlyOwner() {
+        _baseURIextended = baseURI_;
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
+        require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return _baseURIextended;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        string memory _tokenURI = _tokenURIs[tokenId];
+        return _tokenURI;
     }
 
     function award() public returns (uint256) {
         counter++;
         _mint(msg.sender, counter);
         return counter;
-    }
-
-    function tokenURI(uint256 _tokenId)
-        public
-        view
-        override(ERC721, IERC721Metadata)
-        returns (string memory)
-    {
-        require(
-            _exists(_tokenId),
-            "ERC721Metadata: URI query for nonexistent token"
-        );
-        string memory uri = tokenUris[_tokenId];
-        return uri;
     }
 }
