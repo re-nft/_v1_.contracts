5327e291dad0ea8ca2b93abea8f4f4f64c621169
nazariyv <nazariy@inbox.ru>
2021-07-22 10:49:41 +0100

    variable addresses

--- deploy/prod/02_deploy_rentnft.ts
+++ deploy/prod/02_deploy_rentnft.ts
@@ -1,6 +1,12 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 
+const WETH = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+const DAI = "0x6b175474e89094c44da98b954eedeac495271d0f";
+const USDC = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
+const USDT = "0xdac17f958d2ee523a2206206994597c13d831ec7";
+const TUSD = "0x0000000000085d4780B73119b644AE5ecd22b376";
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, ethers, network } = hre;
   const { deploy } = deployments;
@@ -32,27 +38,27 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   if (network.name == "mainnet") {
     const r = await ethers.getContract("Resolver", signer);
     await (
-      await r.setPaymentToken(1, "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", {
+      await r.setPaymentToken(1, WETH, {
         gasPrice: GAS_PRICE,
       })
     ).wait();
     await (
-      await r.setPaymentToken(2, "0x6b175474e89094c44da98b954eedeac495271d0f", {
+      await r.setPaymentToken(2, DAI, {
         gasPrice: GAS_PRICE,
       })
     ).wait();
     await (
-      await r.setPaymentToken(3, "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", {
+      await r.setPaymentToken(3, USDC, {
         gasPrice: GAS_PRICE,
       })
     ).wait();
     await (
-      await r.setPaymentToken(4, "0xdac17f958d2ee523a2206206994597c13d831ec7", {
+      await r.setPaymentToken(4, USDT, {
         gasPrice: GAS_PRICE,
       })
     ).wait();
     await (
-      await r.setPaymentToken(5, "0x0000000000085d4780B73119b644AE5ecd22b376", {
+      await r.setPaymentToken(5, TUSD, {
         gasPrice: GAS_PRICE,
       })
     ).wait();
